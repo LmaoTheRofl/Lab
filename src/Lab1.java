339d4c96e9c85948af94ab61eea47b22d3589270
LmaoTheRofl <Zharkov.nik04@mail.ru>
2022-09-16 12:42:21 +0300

    AY

--- src/Lab1.java
+++ src/Lab1.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 public class Lab1 {
     public static void main(String[] args) {
         long[] c = new long[10];
@@ -8,31 +6,29 @@ public class Lab1 {
         c[b]=j;
         b++;
     }}
-        System.out.println(Arrays.toString(c));
-       double[] x = new double[20];
-       for (int i = 0; i<20; i++) {
+       double[] x = new double[18];
+       for (int i = 0; i<18; i++) {
            double num = (Math.random()*19-5);
            x[i] = num;
-       }System.out.println(Arrays.toString(x));
-       double[][] d = new double[10][20];
+       }
+       double[][] d = new double[10][18];
        for (int i = 0; i<10; i++){
-           for (int j = 0; j<20; j++){
-               if (c[i] == 13){
-                   d[i][j] = Math.tan(Math.tan(Math.pow(x[j], ((x[j]+1/3)/3))));
+           for (int j = 0; j<18; j++){
+               if (c[i] == 6){
+                   d[i][j] = Math.pow(((Math.pow((x[j]+1)/x[j], 2))/2), 2)*2;
                }
-               else if (c[i]==6 | c[i]==9 | c[i]==13 | c[i]==14 | c[i]==15) {
-                   d[i][j] = Math.pow(2*Math.tan(Math.atan((x[j]+1)/22)), (3/4)/(2/((2/3)+x[j])));
+               else if (c[i]==4 | c[i]==10 | c[i]==16 | c[i]==18 | c[i]==20) {
+                   d[i][j] = Math.cos(Math.sin(Math.cos(x[j])));
                }
                else {
-
-                   d[i][j] = Math.pow(Math.asin(Math.cos(Math.log(Math.pow(Math.E, x[j])))), 3);
+                   d[i][j] = Math.atan(Math.cos(Math.asin(Math.pow(Math.E, Math.pow(-1*(Math.pow((5*(Math.abs(x[j])+1)), 2)), 1/3)))));
                }
 
            }
        }
         for (int i = 0; i<10; i++){
-            for (int j = 0; j<20; j++){
-                System.out.printf("%.3f", d[i][j]);
+            for (int j = 0; j<18; j++){
+                System.out.printf("%.2f", d[i][j]);
                 System.out.print(" ");
             }
             System.out.println();
